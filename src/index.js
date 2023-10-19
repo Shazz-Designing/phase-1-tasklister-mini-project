b98b8b6cfb6a22999e3542d1bcea42c39afa7506
sharon <sapot@safaricom.co.ke>
2023-10-19 22:38:21 +0300

    Add js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    const taskDescription = taskInput.value;
+
+    if (taskDescription) {
+     
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+
+      taskList.appendChild(taskItem);
+
+      taskInput.value = "";
+    }
+  });
 });
+
